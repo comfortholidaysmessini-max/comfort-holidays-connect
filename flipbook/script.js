783d0a4ff6dc85baa5cf820d69d8379001e2851d
comfortholidaysmessini-max <comfortholidays.messini@gmail.com>
2026-05-03 17:27:44 +0300

    Update script.js

--- flipbook/script.js
+++ flipbook/script.js
@@ -1,12 +1,21 @@
-var swiper = new Swiper('.swiper-container', {
+/* FIX REAL FULLSCREEN HEIGHT ON MOBILE */
+function fixHeight() {
+    const realHeight = window.innerHeight;
+    document.documentElement.style.setProperty('--real-height', realHeight + 'px');
+}
+
+fixHeight();
+window.addEventListener('resize', fixHeight);
+
+/* SWIPER INITIALIZATION */
+const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: false,
+    grabCursor: true,
     spaceBetween: 0,
-    centeredSlides: true,
-    slidesPerView: 1,
     on: {
         slideChange: function () {
-            document.getElementById("current").textContent = swiper.activeIndex + 1;
+            document.getElementById("current").textContent = this.activeIndex + 1;
         }
     }
 });
